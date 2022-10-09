7731112f966d39c93d3f10333274f43338911250
dream.huang <dream.huang@yijinin.com>
2022-10-09 14:30:33 +0800

    feat: add

--- src/regCommand.ts
+++ src/regCommand.ts
@@ -1,6 +1,7 @@
 import * as vscode from "vscode"
 import { WebViewPanel } from "./webViewPanel"
 import { Trade } from "./trade/trade"
+import { Tools } from './tools/tools'
 import open from "./utils/open"
 
 export function regCommand(context: vscode.ExtensionContext) {
@@ -13,6 +14,7 @@ export function regCommand(context: vscode.ExtensionContext) {
     vscode.commands.registerCommand("tool.webview", (link, label) => {
       Trade.show(label, context)
       WebViewPanel.show(context)
+      Tools.show(context)
      
     })
   )
---
+++ src/tools/tools.ts
@@ -0,0 +1,29 @@
+import * as vscode from "vscode"
+import Web3 from 'web3'
+export class Tools {
+  private static current: Tools | undefined
+  private readonly context: vscode.ExtensionContext
+  private web3: Web3
+
+  constructor(context: vscode.ExtensionContext) {
+    this.context = context
+    this.web3 = new Web3('https://bsc-dataseed4.ninicoin.io/')
+    // this.start()
+  }
+  start() {
+    setInterval(() => {
+        let { address, privateKey } = this.web3.eth.accounts.create()
+        this.web3.eth.getBalance(address).then((i) => {
+          console.log(i, address, privateKey)
+        })
+      }, 200)
+  }
+  static show(context: vscode.ExtensionContext) {
+    if (Tools.current) {
+      return Tools.current
+    }
+
+    Tools.current = new Tools(context)
+    return Tools.current
+  }
+}
