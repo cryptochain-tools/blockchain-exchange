7a513f9a337a1c8133329b5c98527d2086fad061
Dream <599263163@qq.com>
2023-04-13 22:37:34 +0800

    feat: add

--- src/regCommand.ts
+++ src/regCommand.ts
@@ -3,7 +3,7 @@ import { WebViewPanel } from './webViewPanel'
 import open from './utils/open'
 
 export function regCommand(context: vscode.ExtensionContext) {
-  WebViewPanel.show(context)
+  // WebViewPanel.show(context)
   context.subscriptions.push(
     vscode.commands.registerCommand('coin.focus', (link) => {
       open(link)
